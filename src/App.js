ed8e53d7575a3a7f2e77468320e2906dce940d4b
ghazanfarshah <ghazanfar.shah@nextgeni.com>
2024-10-09 15:08:07 +0500

    changes
    react version downgraded to 17
    react dom scripts version 5
    routers added
    new place page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 const App = () => {
   return (
     <Router>
-      <Route path = "/" exact>
-        <Users />
-      </Route>
+      <Switch>
+        <Route path = "/" exact>
+          <Users />
+        </Route>
+        <Route path = "/place/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 }
