a3ed499e74a5dd42f6dda3d7e3934485b03e1bbe
ghazanfarshah <ghazanfar.shah@nextgeni.com>
2024-10-10 16:29:17 +0500

    headers and navigations implementation started

---
+++ src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+import './MainNavigation.css';
+import MainHeader from "../MainHeader/MainHeader";
+
+const MainNavigation = props => {
+    return (
+        <MainHeader>
+            <button className="main-navigation__menu-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <h1 className="main_navigation__title">
+                <Link to="/" style={{ textDecoration: 'none', color: 'white' }} >Your Places</Link>
+            </h1>
+            <nav>
+                ...
+            </nav>
+        </MainHeader>
+    )
+}
+
+export default MainNavigation;
